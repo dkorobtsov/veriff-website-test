8835f27a2a12aea11f0fda7a039df020873f05b5
Dmitri Korobtsov <dmitri.korobtsov@gmail.com>
2019-08-18 01:19:37 +0300

    Added info logs to gradle output (otherwise it feels like process is hanging).

--- tests.sh
+++ tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --network=host --rm -v "${PWD}/.gradle:/home/gradle/.gradle" -v "$PWD":/home/gradle/project -v "$PWD"/allure-results:/home/gradle/project/allure-results -w /home/gradle/project gradle:latest gradle --no-daemon test
+docker run --network=host --rm -v "${PWD}/.gradle:/home/gradle/.gradle" -v "$PWD":/home/gradle/project -v "$PWD"/allure-results:/home/gradle/project/allure-results -w /home/gradle/project gradle:latest gradle --no-daemon --info test
