3cc99edf1005fb042da36fb81cd6c13a56d6fb12
Dmitri Korobtsov <dmitri.korobtsov@gmail.com>
2019-08-19 01:27:13 +0300

    Adjusting general configuration. Added property file, debug disabled by default.

--- src/main/kotlin/com/dkorobtsov/veriff/website/test/config/SuiteConfig.kt
+++ src/main/kotlin/com/dkorobtsov/veriff/website/test/config/SuiteConfig.kt
@@ -6,17 +6,30 @@ import org.aeonbits.owner.Reloadable
 @Config.Sources("classpath:suite.properties")
 interface SuiteConfig : Reloadable {
 
-    @Config.DefaultValue("https://www.veriff.com")
-    fun baseUrl(): String
+    /**
+     * if debug mode is enabled, page screenshots and diff from baseline (if any)
+     * will be saved to /screenshots folder
+     */
+    @Config.DefaultValue("true")
+    fun isDebug(): Boolean
 
+    /**
+     * NB. Current configuration supports chrome only.
+     *
+     * To add another browser need to perform following actions:
+     * <pre>
+     * 1. adjust config/browsers.json
+     * 2. pull browser container
+     * </pre>
+     */
     @Config.DefaultValue("chrome")
     fun targetBrowser(): String
 
+    @Config.DefaultValue("https://www.veriff.com")
+    fun baseUrl(): String
+
     @Config.DefaultValue("false")
     fun isHeadless(): Boolean
-
-    @Config.DefaultValue("true")
-    fun isDebug(): Boolean
 
     @Config.DefaultValue("http://localhost:4444/wd/hub")
     fun selenoidUrl(): String
