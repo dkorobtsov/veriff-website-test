b1345c69cdbaa283b294c78bb423023b6dbc79d4
Dmitri Korobtsov <dmitri.korobtsov@gmail.com>
2019-08-25 02:20:53 +0300

    Fixing getRedirectUrl response.

--- src/main/kotlin/com/dkorobtsov/veriff/website/test/urlcheck/UrlVerifier.kt
+++ src/main/kotlin/com/dkorobtsov/veriff/website/test/urlcheck/UrlVerifier.kt
@@ -28,11 +28,10 @@ class UrlVerifier {
         val huc = URL(url).openConnection() as HttpURLConnection
         huc.instanceFollowRedirects = false
         huc.connect();
-        val responseCode = when (huc.responseCode) {
-            in 300..399 -> "unknown location"
-            else -> "request code (${huc.responseCode}) is not a redirect"
-        }
-        return huc.getHeaderField("Location") ?: "Unknown"
+        return huc.getHeaderField("Location")
+            ?: when (huc.responseCode) {
+                in 300..399 -> "Unknown Location"
+                else -> "request code (${huc.responseCode}) is not a redirect"
+            }
     }
-
 }
